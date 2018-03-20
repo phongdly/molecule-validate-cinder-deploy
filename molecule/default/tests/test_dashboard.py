5e40de164f3bd3cecc370a0f754c1f49a51137b9
phongdly <phongdly@gmail.com>
2018-03-20 12:08:23 -0700

    RO-4091 updated find openrc

--- molecule/default/tests/test_dashboard.py
+++ molecule/default/tests/test_dashboard.py
@@ -10,17 +10,20 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 @pytest.mark.jira('asc-222')
 # Experimental test
 def test_find_openrc_on_root(host):
-    assert host.file("/root/openrc").exists
+    cmd = "find /root -name openrc -print | grep openrc"
+    assert host.run(cmd)
 
 
 # Experimental test
 def test_find_openrc_on_opt(host):
-    assert host.file("/opt/openrc").exists
+    cmd = "find /opt -name openrc -print | grep openrc"
+    assert host.run(cmd)
 
 
 # Experimental test
 def test_find_openrc_on_etc(host):
-    assert host.file("/etc/openrc").exists
+    cmd = "find /etc -name openrc -print | grep openrc"
+    assert host.run(cmd)
 
 
 # Experimental test
