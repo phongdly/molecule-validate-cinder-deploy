c9f4bff388f821691d15651a57408fd8da6ef0ef
phongdly <phongdly@gmail.com>
2018-03-20 11:21:47 -0700

    RO-4091 updated experimental tests

--- molecule/default/tests/test_dashboard.py
+++ molecule/default/tests/test_dashboard.py
@@ -8,39 +8,40 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 @pytest.mark.jira('asc-222')
-def test_cinder_service_on_all(host):
-    """Test to verify that cinder service is running on the cinder nodes
-
-    Args:
-        host(testinfra.host.Host): A hostname in dynamic_inventory.json/molecule.yml
-    """
-
-    if not host.file('/root/openrc').exists:
-        raise Exception('openrc file not found')
-    elif host.file('/root/openrc').exists:
-        cmd = "sudo bash -c \"source /root/openrc; cinder service-list\""
-        output = host.run(cmd)
-        if output.rc != 0:
-            raise Exception('cinder client is not available')
-        elif output.rc == 0:
-            assert ("cinder-volume" in output.stdout)
+# Experimental test
+def test_find_openrc_on_root(host):
+    assert host.file("/root/openrc").exists
 
 
-# Experimental tess
-def test_cinder_api_status_on_all_host(host):
+# Experimental test
+def test_find_openrc_on_opt(host):
+    assert host.file("/opt/openrc").exists
+
+
+# Experimental test
+def test_find_openrc_on_etc(host):
+    assert host.file("/etc/openrc").exists
+
+# Experimental test
+def test_find_cinder_client(host):
+    assert host.exists("cinder")
+
+
+# Experimental test
+def test_cinder_api_status(host):
     assert host.service('cinder-api').is_running
 
 
-# Experimental tess
-def test_cinder_schuduler_status_on_all_host(host):
+# Experimental test
+def test_cinder_schuduler_status(host):
     assert host.service('cinder-scheduler').is_running
 
 
-# Experimental tess
-def test_cinder_volume_status_on_all_host(host):
+# Experimental test
+def test_cinder_volume_status(host):
     assert host.service('cinder-volume').is_running
 
 
-# Experimental tess
-def test_cinder_backup_status_on_all_host(host):
+# Experimental test
+def test_cinder_backup_status(host):
     assert host.service('cinder-backup').is_running
